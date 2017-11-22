5d047b14702a111b449401d6e98e2039a092f510
TaChyla <tachylad.murray@gmail.com>
2017-11-21 19:23:12 -0800

    added test folder & test suite requirements

---
+++ test/cities-test.js
@@ -0,0 +1,18 @@
+/* The city search box. 
+* City search box element exists
+* Users can type in input box; cursor
+* Autocomplete pops out acknowledging text
+* Users can fully type city, state & select enter 
+* Users can opt to select Google’s autocomplete
+* Enter captures input 
+* No errors 
+* The cities list. 
+* States are Listed
+* Cities are accurately listed under State 
+* Clicking on city routes user to lyft.com/cities/cityName || Area (Ex. /greater-toronto-area-on for Ajax
+* Cities are accurately listed under Province
+* States in alphabetical order
+* Cities in alphabetical order
+
+* The city details page. (Each city’s details)*/
+
