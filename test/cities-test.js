1c3af864f951bdc77f264b2dd6ee5b2a3af1a989
TaChyla <tachylad.murray@gmail.com>
2017-11-21 20:02:16 -0800

    positive test suite

--- test/cities-test.js
+++ test/cities-test.js
@@ -1,18 +1,49 @@
-/* The city search box. 
-* City search box element exists
-* Users can type in input box; cursor
-* Autocomplete pops out acknowledging text
-* Users can fully type city, state & select enter 
-* Users can opt to select Google’s autocomplete
-* Enter captures input 
-* No errors 
-* The cities list. 
-* States are Listed
-* Cities are accurately listed under State 
-* Clicking on city routes user to lyft.com/cities/cityName || Area (Ex. /greater-toronto-area-on for Ajax
-* Cities are accurately listed under Province
-* States in alphabetical order
-* Cities in alphabetical order
+/* 
+City Search Box
+    -City search box element exists
+    -Users can type in input box
+    -Autocomplete interactivly acknowledges text
+    -Users can fully type city, stare & select enter
+    -Users can opt to select Google's autocomplete
+    -Enter captures input
+    -No errors display when typing 
 
-* The city details page. (Each city’s details)*/
+Cities list
+* States are Listed
+* States in alphabetical order
+* Cities are accurately listed under State 
+* Cities are accurately listed under Province
+* Cities in alphabetical order
+* Clicking on city routes user to lyft.com/cities/cityName || Area (Ex. /greater-toronto-area-on for Ajax
+* tab displays correct "cities-city || region"
+
+City details page (Each city’s details)
+
+    -City Image displays
+    -h1 el displays city || region name 
+    -signup now button exists
+    -clicking signup now lead user to authentication
+    -description is for selected city||region
+    -region of coverate highlighted on map is accurate
+    -users can zoom in/out map
+    -Get ride estimation element exists
+    -Add pickup location input element exists
+    -Add destination input element exists
+    
+The estimate ride cost form on the details page
+    -price should be consistent regardless of location entered (negative case)
+        ex. atlanta page but enters san francisco locations=> price should be the same 
+    -price estimate displays when get estimate element is clicked
+    -sign up and ride button displays when 'Get Estimate' button is clicked
+    -Signup & ride leads user to authentication
+
+        (Negatives) Error when fields are left blank
+        (Negative) cannot proceed when fields are empty
+
+
+
+
+
+
+*/
 
